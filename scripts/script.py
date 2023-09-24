36ff2b1514387c0d09683c682fcfe28f8375eed7
zhandos998 <61329282+zhandos998@users.noreply.github.com>
2023-09-24 23:59:20 +0600

    24092023

--- scripts/script.py
+++ scripts/script.py
@@ -1,48 +1,34 @@
 import pyvista as pv
 import pandas as pd
-import numpy as np
 import os
+import video 
 
-dir_path = 'C:\\Users\\zhandos998\\Desktop\\julia\\data\\'
-filenames = []
+project_path = 'C:/github_repositories/medicine/julia/'
 
 plotter = pv.Plotter()
-plotter.open_movie('movie.mp4')  
-# Создание объекта PyVista
-point_cloud = pv.PolyData()
+# plotter = pv.Plotter(off_screen=True)
+def plotting(csv_path,image_path,first = False):
+    data = pd.read_csv(csv_path)
+    point_cloud = pv.PolyData()
+    point_cloud.points = data[['x1', 'x2', 'u']].values
+    point_cloud['u'] = data['u'].values
+    delaunay = point_cloud.delaunay_2d()
+    plotter.add_mesh(delaunay, scalars='u', cmap='jet')
+    # if first:
+    plotter.camera_position = 'xy'
+    plotter.camera.roll=180
+    plotter.show(auto_close=False, interactive=False,screenshot=image_path)
+    return image_path
 
-# for i in range(0,100001,1000):
-for i in range(0,10001,1000):
-    # check if current file_path is a file
-    file_path = 'data.{}.csv'.format(i)
-    # print(file_path)
-    if os.path.isfile(os.path.join(dir_path, file_path)):
-        # Загрузка CSV-файла
-        data = pd.read_csv('C:\\Users\\zhandos998\\Desktop\\julia\\data\\' + file_path)
-        print(file_path)
+# plotting(project_path+'data/data.0.csv',project_path+'frames2/frame_0.jpeg',True)
+frames=[]
 
-        # Установка координатных данных
-        point_cloud.points = data[['x1', 'x2', 'u']].values
-        point_cloud['u'] = data['u'].values
+t = 0
+# for i in range(0000,100001,1000):
+for i in range(0000+10000*t,10001+10000*t,1000):
+    file_path = 'data/data.{}.csv'.format(i)
+    print(file_path)
+    if os.path.isfile(os.path.join(project_path, file_path)):
+        frames.append(plotting(project_path + file_path, project_path +'frames2/frame_{}.jpeg'.format(i)))        
 
-        delaunay = point_cloud.delaunay_2d()
-
-        plotter.add_mesh(delaunay, color='blue', scalars='u', cmap='jet')
-
-        plotter.camera_position = 'xy'
-        plotter.camera.roll=180
-        for _ in range(10):
-            plotter.write_frame()  
-        # pv.plot(record='movie.mp4', off_screen=True, window_size=(800, 600), interactive=False, fps=fps)
-
-        # plotter.show()
-
-# output_video_file = "output_video.mp4"
-
-# # Define the frame rate (fps)
-# fps = 10
-
-# # Render the scene as an MP4 video
-# pv.plot(record=output_video_file, off_screen=True, window_size=(800, 600), interactive=False, fps=fps)
-
-plotter.close()
+video.record_video(image_folder='C:/github_repositories/medicine/julia/frames2/',fps=5,video_name='output_video2.mp4',images=frames)
